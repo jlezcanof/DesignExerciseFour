11e476c51b8c9f12bb113848671562284cffd8ce
lezcanin <reymidas76@yahoo.es>
2025-03-23 11:40:09 +0100

    Initial Commit

---
+++ DesignExerciseFour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignExerciseFour
+//
+//  Created by Yomismista on 23/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignExerciseFour/DesignExerciseFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignExerciseFourApp.swift
+//  DesignExerciseFour
+//
+//  Created by Yomismista on 23/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DesignExerciseFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
